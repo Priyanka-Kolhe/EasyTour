6ada1ce08cc85f4fbe13da561ec7e335a297f11d
priyanka-kolhe <pkolhe657@gmail.com>
2025-06-23 18:25:34 +0530

    sociallinks added

--- src/components/Sociallinks/Sociallink.jsx
+++ src/components/Sociallinks/Sociallink.jsx
@@ -5,18 +5,11 @@ const sociallink = () => {
   return (
     <>
         <div className="social-links">
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-            <i className="ri-facebook-fill"></i>
-            </a>
-            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-            <i className="ri-twitter-fill"></i>
-            </a>
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-            <i className="ri-instagram-fill"></i>
-            </a>
-            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-            <i className="ri-linkedin-fill"></i>
-            </a>
+
+            <a href="https://github.com/Priyanka-Kolhe"> <i class="ri-github-fill"></i></a>
+
+            <a href="https://www.linkedin.com/in/priyanka-kolhe-aba0ba2b2/"><i class="ri-linkedin-box-fill"></i></a>
+        
         </div>
     </>
   )
